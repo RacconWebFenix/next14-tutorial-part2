a3bc3e2dcf424f825c1d21a6f8f1833f40d28b84
Marcos César Domingues <dfenixweb@gmail.com>
2024-04-24 15:39:23 -0300

    cookies in api with next

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -9,9 +9,17 @@ export async function GET(request: NextRequest) {
   console.log(requestHeaders.get("Authorization"));
 
   console.log(headersList.get("Authorization"));
+
+  cookies().set("resultsPerPage", "20");
+  const theme = request.cookies.get("theme");
+
+  console.log(theme);
+  console.log(cookies().getAll());
+
   return new Response("<h1>Hello profile!</h1>", {
     headers: {
       "Content-type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
