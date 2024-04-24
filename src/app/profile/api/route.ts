045a82ef83280e57075494c675de92a909fe1bc4
Marcos César Domingues <dfenixweb@gmail.com>
2024-04-24 15:30:54 -0300

    handle route heades

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-  return new Response("Hello profile!");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+
+  const headersList = headers();
+
+  console.log(requestHeaders.get("Authorization"));
+
+  console.log(headersList.get("Authorization"));
+  return new Response("<h1>Hello profile!</h1>", {
+    headers: {
+      "Content-type": "text/html",
+    },
+  });
 }
